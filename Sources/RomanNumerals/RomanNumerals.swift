d8da52723a2b747e407cdb5295ff2b3ad8140088
el aidi <hamza.el-aidi@olbati.com>
2019-08-09 14:27:06 +0200

    use SPM

---
+++ Sources/RomanNumerals/RomanNumerals.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+class RomanNumerals {
+    
+    let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    let romanValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+    
+    func convertToRoman(value number: Int) -> String {
+        
+        var romanResult = ""
+        var _number = number
+        
+        for (index, roman) in romanValues.enumerated() {
+            let value = values[index]
+            let result = _number / value
+            
+            if result > 0 {
+                for _ in 0..<result {
+                    romanResult += roman
+                }
+                _number -= value * result
+            }
+        }
+        
+        return romanResult
+    }
+}
---
+++ Tests/RomanNumeralsTests/RomanNumeralsTests.swift
@@ -0,0 +1,39 @@
+import XCTest
+@testable import RomanNumerals
+
+final class RomanNumeralsTests: XCTestCase {
+    
+    let romanNumeral = RomanNumerals()
+    
+    func test_should_return_I_whene_value_is_1() {
+        XCTAssertEqual("I", romanNumeral.convertToRoman(value: 1))
+    }
+    
+    func test_should_return_V_whene_value_is_5() {
+        XCTAssertEqual("V", romanNumeral.convertToRoman(value: 5))
+    }
+    
+    func test_should_return_X_whene_value_is_10() {
+        XCTAssertEqual("X", romanNumeral.convertToRoman(value: 10))
+    }
+    
+    func test_should_return_L_whene_value_is_50() {
+        XCTAssertEqual("L", romanNumeral.convertToRoman(value: 50))
+    }
+    
+    func test_should_return_C_whene_value_is_100() {
+        XCTAssertEqual("C", romanNumeral.convertToRoman(value: 100))
+    }
+    
+    func test_should_return_D_whene_value_is_500() {
+        XCTAssertEqual("D", romanNumeral.convertToRoman(value: 500))
+    }
+    
+    func test_should_return_M_whene_value_is_1000() {
+        XCTAssertEqual("M", romanNumeral.convertToRoman(value: 1000))
+    }
+
+    static var allTests = [
+        ("test_should_return_I_whene_value_is_1", test_should_return_I_whene_value_is_1),
+    ]
+}
